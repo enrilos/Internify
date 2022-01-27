413f99f26d1deb903c63034b08702b3ba90b314f
emrrar <avatareigan@gmail.com>
2022-01-27 19:43:50 +0200

    Initialized ASP.NET Core MVC project.

---
+++ Internify/Web/Internify.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Internify.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
