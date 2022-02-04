00345f272bf4827afe15f777f36593c3603db0b8
emrrar <avatareigan@gmail.com>
2022-02-04 20:09:47 +0200

    Added DataConstants.cs where model constants are stored.

---
+++ Internify/Data/Internify.Data.Common/DataConstants.cs
@@ -0,0 +1,18 @@
+﻿namespace Internify.Data.Common
+{
+    public class DataConstants
+    {
+        public const string SQLCharType = "char";
+        public const int IdGuidLength = 36;
+
+        public class ApplicationUser
+        {
+            public const int NameMaxLength = 50;
+        }
+
+        public class Candidate
+        {
+            public const int StudyFieldMaxLength = 40;
+        }
+    }
+}
