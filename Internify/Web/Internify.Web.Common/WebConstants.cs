49215130aee764411982388f936876cb223c5f4c
emrrar <avatareigan@gmail.com>
2022-02-04 14:48:22 +0200

    Added WebConstants.cs file with all roles in it.

---
+++ Internify/Web/Internify.Web.Common/WebConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace Internify.Web.Common
+{
+    public class WebConstants
+    {
+        public const string AdministratorRoleName = "Administrator";
+        public const string CandidateRoleName = "Candidate";
+        public const string CompanyRoleName = "Company";
+        public const string UniversityRoleName = "University";
+    }
+}
