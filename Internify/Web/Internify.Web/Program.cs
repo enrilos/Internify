b5cdc0bed584d9e8ecd9689e7df16b8da9d1d301
emrrar <avatareigan@gmail.com>
2022-02-06 14:23:00 +0200

    Added Web project.

--- Internify/Web/Internify.Web/Program.cs
+++ Internify/Web/Internify.Web/Program.cs
@@ -1,4 +1,5 @@
 using Internify.Data;
+using Internify.Data.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,17 +7,36 @@ var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-builder.Services.AddDbContext<InternifyDbContext>(options =>
-    options.UseSqlServer(connectionString));
+builder.Services
+    .AddDbContext<InternifyDbContext>(options => options.UseSqlServer(connectionString));
 
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services
+    .AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services
+    .AddDefaultIdentity<ApplicationUser>(options =>
+    {
+        options.Password.RequiredLength = 6;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireDigit = false;
+        options.Password.RequireUppercase = false;
+    })
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<InternifyDbContext>();
 
-builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+builder.Services
+    .AddControllersWithViews()
+    .AddRazorRuntimeCompilation();
+
+builder.Services
+    .AddAutoMapper(typeof(Program));
+
+// IoC...
 
 var app = builder.Build();
+
+// TODO: Extensions...
+// app.PrepareDatabase();
 
 if (app.Environment.IsDevelopment())
 {
@@ -39,6 +59,15 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
+
 app.MapRazorPages();
 
 app.Run();
