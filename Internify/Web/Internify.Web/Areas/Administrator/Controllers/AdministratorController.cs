3a24e4fcec8ecb801decba8f0c5a11d9824599b7
emrrar <avatareigan@gmail.com>
2022-02-07 10:46:13 +0200

    Added Administrator area.

---
+++ Internify/Web/Internify.Web/Areas/Administrator/Controllers/AdministratorController.cs
@@ -0,0 +1,14 @@
+﻿namespace Internify.Web.Areas.Administrator.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    using static Common.WebConstants;
+
+    [Area(AdministratorRoleName)]
+    [Authorize(Roles = AdministratorRoleName)]
+    [Route($"{AdministratorRoleName}/[controller]/[action]/{{id?}}")]
+    public abstract class AdministratorController : Controller
+    {
+    }
+}
