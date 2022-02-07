49dc3bd84579eb9fc6031c98e6599a12c1f85dfd
emrrar <avatareigan@gmail.com>
2022-02-07 12:21:03 +0200

    Refactored Areas a bit.

---
+++ Internify/Web/Internify.Web/Areas/Candidate/Controllers/CandidateController.cs
@@ -0,0 +1,14 @@
+﻿namespace Internify.Web.Areas.Candidate.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    using static Common.WebConstants;
+
+    [Area(CandidateRoleName)]
+    [Authorize(Roles = CandidateRoleName)]
+    [Route($"{CandidateRoleName}/[controller]/[action]/{{id?}}")]
+    public abstract class CandidateController : Controller
+    {
+    }
+}
