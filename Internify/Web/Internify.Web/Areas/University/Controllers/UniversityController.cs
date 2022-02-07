3ceb8aee3fb10fabcf761526e585c4009ee68133
emrrar <avatareigan@gmail.com>
2022-02-07 10:45:42 +0200

    Added University area.

---
+++ Internify/Web/Internify.Web/Areas/University/Controllers/UniversityController.cs
@@ -0,0 +1,14 @@
+﻿namespace Internify.Web.Areas.University.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    using static Common.WebConstants;
+
+    [Area(UniversityRoleName)]
+    [Authorize(Roles = UniversityRoleName)]
+    [Route($"{UniversityRoleName}/[controller]/[action]/{{id?}}")]
+    public abstract class UniversityController : Controller
+    {
+    }
+}
