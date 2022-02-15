08db5ab7b51be19c2a068a8e4c4e03302eaf817a
emrrar <avatareigan@gmail.com>
2022-02-15 17:37:49 +0200

    Moved role names in AppConstants.cs

---
+++ Internify/Common/Internify.Common/AppConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace Internify.Common
+{
+    public class AppConstants
+    {
+        public const string AdministratorRoleName = "Administrator";
+        public const string CandidateRoleName = "Candidate";
+        public const string CompanyRoleName = "Company";
+        public const string UniversityRoleName = "University";
+    }
+}
