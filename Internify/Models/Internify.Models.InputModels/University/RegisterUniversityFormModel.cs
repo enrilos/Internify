6551efb9864dc2bc01d2b458ddf25c5f8abbf951
emrrar <avatareigan@gmail.com>
2022-02-24 16:54:19 +0200

    Added RegisterUniversityFormModel

---
+++ Internify/Models/Internify.Models.InputModels/University/RegisterUniversityFormModel.cs
@@ -0,0 +1,34 @@
+﻿namespace Internify.Models.InputModels.University
+{
+    using System.ComponentModel.DataAnnotations;
+    using ViewModels.Country;
+
+    using static Data.Common.DataConstants;
+
+    public class RegisterUniversityFormModel
+    {
+        [Required]
+        [StringLength(NameMaxLength, MinimumLength = NameMinLength)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(UrlMaxLength)]
+        [Display(Name = "Image URL")]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [MaxLength(UrlMaxLength)]
+        [Display(Name = "Website URL")]
+        public string WebsiteUrl { get; set; }
+
+        [Required]
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Invalid option.")]
+        [Display(Name = "Country")]
+        public string CountryId { get; set; }
+
+        public IEnumerable<CountryListingViewModel> Countries { get; set; }
+    }
+}
