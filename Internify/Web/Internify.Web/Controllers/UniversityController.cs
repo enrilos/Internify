9317ad25440ea6adc3a49983428c369a45c884d0
emrrar <avatareigan@gmail.com>
2022-02-24 16:56:42 +0200

    University addition works correctly.

--- Internify/Web/Internify.Web/Controllers/UniversityController.cs
+++ Internify/Web/Internify.Web/Controllers/UniversityController.cs
@@ -2,18 +2,34 @@
 {
     using Common;
     using Infrastructure.Extensions;
+    using Internify.Models.InputModels.University;
+    using Internify.Models.ViewModels.Country;
+    using Services.University;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Caching.Memory;
+    using Services.Country;
+
+    using static Common.WebConstants;
 
     public class UniversityController : Controller
     {
+        private readonly IUniversityService universityService;
+        private readonly ICountryService countryService;
+        private readonly IMemoryCache cache;
         private readonly RoleChecker roleChecker;
 
-        // TODO: Consider making ApiController that works with dynamic search bar functionality here.
-        // Search university by name dynamically.
-
-        public UniversityController(RoleChecker roleChecker)
-            => this.roleChecker = roleChecker;
+        public UniversityController(
+            IUniversityService universityService,
+            ICountryService countryService,
+            IMemoryCache cache,
+            RoleChecker roleChecker)
+        {
+            this.universityService = universityService;
+            this.countryService = countryService;
+            this.cache = cache;
+            this.roleChecker = roleChecker;
+        }
 
         [Authorize]
         public IActionResult Register()
@@ -23,7 +39,79 @@
                 return BadRequest();
             }
 
-            return View();
+            var universityModel = new RegisterUniversityFormModel
+            {
+                Countries = AcquireCachedCountries()
+            };
+
+            return View(universityModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Register(RegisterUniversityFormModel university)
+        {
+            var userId = User.Id();
+
+            if (User.IsAdmin() || roleChecker.IsUserInAnyRole(userId))
+            {
+                return BadRequest();
+            }
+
+            if (!countryService.Exists(university.CountryId))
+            {
+                ModelState.AddModelError(nameof(university.CountryId), "Invalid option.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                university.Countries = AcquireCachedCountries();
+
+                return View(university);
+            }
+
+            var universityId = universityService.Add(
+                userId,
+                university.Name,
+                university.ImageUrl,
+                university.WebsiteUrl,
+                university.Description,
+                university.CountryId);
+
+            TempData[GlobalMessageKey] = "Thank you for registering your university!";
+
+            return RedirectToAction(nameof(Details), new { id = universityId });
+        }
+
+        public IActionResult Details(string id)
+        {
+            // TODO: Implement details view model.
+            //var university = universityService.GetDetailsModel(id);
+
+            //if (university == null)
+            //{
+            //    return NotFound();
+            //}
+
+            //return View(university);
+            return null;
+        }
+
+        private IEnumerable<CountryListingViewModel> AcquireCachedCountries()
+        {
+            var countries = cache.Get<IEnumerable<CountryListingViewModel>>(CountriesCacheKey);
+
+            if (countries == null)
+            {
+                countries = countryService.All();
+
+                var cacheOptions = new MemoryCacheEntryOptions()
+                    .SetPriority(CacheItemPriority.NeverRemove);
+
+                cache.Set(CountriesCacheKey, countries, cacheOptions);
+            }
+
+            return countries;
         }
     }
 }
