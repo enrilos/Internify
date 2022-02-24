30529fa61fadd2f638fbe975e7794d9deb13d120
emrrar <avatareigan@gmail.com>
2022-02-24 17:25:42 +0200

    Added UniversityDetailsViewModel

---
+++ Internify/Models/Internify.Models.ViewModels/University/UniversityDetailsViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Internify.Models.ViewModels.University
+{
+    public class UniversityDetailsViewModel
+    {
+        public string Id { get; init; }
+
+        public string Name { get; init; }
+
+        public string ImageUrl { get; init; }
+
+        public string WebsiteUrl { get; init; }
+
+        public string Description { get; init; }
+
+        public string Country { get; init; }
+    }
+}
