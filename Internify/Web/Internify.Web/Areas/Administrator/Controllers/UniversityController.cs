56044500c2391271d588b097e93707219e3b0ea9
emrrar <avatareigan@gmail.com>
2022-02-27 14:49:09 +0200

    Admins can delete universities.

---
+++ Internify/Web/Internify.Web/Areas/Administrator/Controllers/UniversityController.cs
@@ -0,0 +1,31 @@
+﻿namespace Internify.Web.Areas.Administrator.Controllers
+{
+    using Services.University;
+    using Microsoft.AspNetCore.Mvc;
+
+    using static Common.WebConstants;
+
+    public class UniversityController : AdministratorController
+    {
+        private readonly IUniversityService universityService;
+
+        public UniversityController(IUniversityService universityService)
+        {
+            this.universityService = universityService;
+        }
+
+        public IActionResult Delete(string id)
+        {
+            var deleteResult = universityService.Delete(id);
+
+            if (!deleteResult)
+            {
+                return BadRequest();
+            }
+
+            TempData[GlobalMessageKey] = "Successfully deleted university.";
+
+            return RedirectToAction("All", "University");
+        }
+    }
+}
