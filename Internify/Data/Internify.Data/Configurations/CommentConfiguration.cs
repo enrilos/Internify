04cea2c71ec9e3df3758163eec708106741b6a3c
emrrar <avatareigan@gmail.com>
2022-02-27 13:05:00 +0200

    Implemented Comment configuration class.

---
+++ Internify/Data/Internify.Data/Configurations/CommentConfiguration.cs
@@ -0,0 +1,24 @@
+﻿namespace Internify.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using Models;
+
+    public class CommentConfiguration : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder
+                .HasOne(x => x.Article)
+                .WithMany(x => x.Comments)
+                .HasForeignKey(x => x.ArticleId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasOne(x => x.Candidate)
+                .WithMany(x => x.Comments)
+                .HasForeignKey(x => x.CandidateId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
