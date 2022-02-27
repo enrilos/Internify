12968581fe90bb9ebe9209128a7750c39ad6eb01
emrrar <avatareigan@gmail.com>
2022-02-27 13:06:01 +0200

    Implemented IAuditInfo and IDeletableEntity in Comment class.

--- Internify/Data/Internify.Data.Models/Comment.cs
+++ Internify/Data/Internify.Data.Models/Comment.cs
@@ -1,10 +1,11 @@
 ﻿namespace Internify.Data.Models
 {
+    using Common;
     using System.ComponentModel.DataAnnotations;
 
     using static Common.DataConstants.Comment;
 
-    public class Comment
+    public class Comment : IAuditInfo, IDeletableEntity
     {
         [Key]
         public string Id { get; init; } = Guid.NewGuid().ToString();
@@ -22,5 +23,13 @@
         public string ArticleId { get; set; }
 
         public Article Article { get; set; }
+
+        public DateTime CreatedOn { get; init; } = DateTime.UtcNow;
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
     }
 }
