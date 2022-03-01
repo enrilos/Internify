04ae13a44eb06e6b0776db623c8216c689bc2feb
emrrar <avatareigan@gmail.com>
2022-03-01 13:32:28 +0200

    Introduced CandidateUniversityService

---
+++ Internify/Services/Internify.Services/CandidateUniversity/CandidateUniversityService.cs
@@ -0,0 +1,57 @@
+﻿namespace Internify.Services.CandidateUniversity
+{
+    using Data;
+    using Data.Models;
+
+    public class CandidateUniversityService : ICandidateUniversityService
+    {
+        private readonly InternifyDbContext data;
+
+        public CandidateUniversityService(InternifyDbContext data)
+        {
+            this.data = data;
+        }
+
+        public bool IsCandidateInUniversityAlumni(string universityId, string candidateId)
+            => data
+            .CandidateUniversities
+            .Any(x =>
+            x.UniversityId == universityId
+            && x.CandidateId == candidateId
+            && !x.IsDeleted);
+
+        public bool AddCandidateToAlumni(string universityId, string candidateId)
+        {
+            var university = data.Universities.FirstOrDefault(x => x.Id == universityId && !x.IsDeleted);
+
+            if (university == null)
+            {
+                return false;
+            }
+
+            var candidate = data.Candidates.FirstOrDefault(x => x.Id == candidateId && !x.IsDeleted);
+
+            if (candidate == null)
+            {
+                return false;
+            }
+
+            var candidateUniversity = new CandidateUniversity
+            {
+                CandidateId = candidate.Id,
+                UniversityId = university.Id
+            };
+
+            data.CandidateUniversities.Add(candidateUniversity);
+
+            data.SaveChanges();
+
+            return true;
+        }
+
+        public bool RemoveCandidateFromAlumni(string universityId, string candidateId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Internify/Services/Internify.Services/CandidateUniversity/ICandidateUniversityService.cs
@@ -0,0 +1,11 @@
+﻿namespace Internify.Services.CandidateUniversity
+{
+    public interface ICandidateUniversityService
+    {
+        public bool IsCandidateInUniversityAlumni(string universityId, string candidateId);
+
+        public bool AddCandidateToAlumni(string universityId, string candidateId);
+
+        public bool RemoveCandidateFromAlumni(string universityId, string candidateId);
+    }
+}
