a931cacee077cf8b051b1a53515916c5e3f4f2c1
emrrar <avatareigan@gmail.com>
2022-03-02 18:13:11 +0200

    Added UniversityListingQueryModel

---
+++ Internify/Models/Internify.Models.InputModels/University/UniversityListingQueryModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Internify.Models.InputModels.University
+{
+    using System.ComponentModel.DataAnnotations;
+    using ViewModels.Country;
+    using ViewModels.University;
+
+    public class UniversityListingQueryModel
+    {
+        public string Name { get; set; }
+
+        [Display(Name = "Country")]
+        public string CountryId { get; set; }
+
+        public IEnumerable<CountryListingViewModel> Countries { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        [Display(Name = "Per Page")]
+        public int UniversitiesPerPage { get; set; }
+
+        public IEnumerable<UniversityListingViewModel> Universities { get; set; }
+
+        public int TotalUniversities { get; set; }
+    }
+}
