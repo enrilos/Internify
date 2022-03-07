736b273cff823026ab2a3d57b207a63452f7a67d
emrrar <avatareigan@gmail.com>
2022-03-07 17:47:39 +0200

    Added CompanyListingViewModel

---
+++ Internify/Models/Internify.Models.ViewModels/Company/CompanyListingViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Internify.Models.ViewModels.Company
+{
+    public class CompanyListingViewModel
+    {
+        public string Id { get; init; }
+
+        public string Name { get; init; }
+
+        public string ImageUrl { get; init; }
+
+        public string Specialization { get; init; }
+
+        public string Country { get; init; }
+    }
+}
