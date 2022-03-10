4b8b65e422de18653969e67c700761068b7ee7fe
emrrar <avatareigan@gmail.com>
2022-03-10 22:03:11 +0200

    Added InternshipListingViewModel

--- Internify/Models/Internify.Models.ViewModels/Internship/InternshipListingViewModel.cs
+++ Internify/Models/Internify.Models.ViewModels/Internship/InternshipListingViewModel.cs
@@ -2,5 +2,16 @@
 {
     public class InternshipListingViewModel
     {
+        public string Id { get; init; }
+
+        public string Role { get; init; }
+
+        public bool IsPaid { get; init; }
+
+        public bool IsRemote { get; init; }
+
+        public string CompanyImageUrl { get; init; }
+
+        public string Country { get; init; }
     }
 }
