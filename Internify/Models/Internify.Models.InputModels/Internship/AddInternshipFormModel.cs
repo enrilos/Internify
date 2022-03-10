767b44ca8a98a136549f015db01aa43aac7257bd
emrrar <avatareigan@gmail.com>
2022-03-10 18:33:42 +0200

    Added AddInternshipFormModel

---
+++ Internify/Models/Internify.Models.InputModels/Internship/AddInternshipFormModel.cs
@@ -0,0 +1,34 @@
+﻿namespace Internify.Models.InputModels.Internship
+{
+    using System.ComponentModel.DataAnnotations;
+    using ViewModels.Country;
+    using ViewModels.Internship;
+
+    using static Data.Common.DataConstants;
+    using static Data.Common.DataConstants.Internship;
+
+    public class AddInternshipFormModel
+    {
+        [Required]
+        [MaxLength(RoleMaxLength)]
+        public string Role { get; set; }
+
+        public bool IsPaid { get; set; }
+
+        [Range(typeof(decimal), "0", "9999999")]
+        public decimal? SalaryUSD { get; set; }
+
+        public bool IsRemote { get; set; }
+
+        [Required]
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; set; }
+
+        public string CompanyId { get; set; }
+
+        [Display(Name = "Country")]
+        public string CountryId { get; set; }
+
+        public IEnumerable<CountryListingViewModel> Countries { get; set; }
+    }
+}
