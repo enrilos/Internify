fe5a27447f175d9c23a99571c822f9cf743b61a7
emrrar <avatareigan@gmail.com>
2022-03-11 20:26:25 +0200

    Moved Company-related internship functionality and views to Company area.

---
+++ Internify/Web/Internify.Web/Areas/Company/Controllers/InternshipController.cs
@@ -0,0 +1,235 @@
+﻿namespace Internify.Web.Areas.Company.Controllers
+{
+    using Infrastructure.Extensions;
+    using Internify.Models.InputModels.Internship;
+    using Internify.Models.ViewModels.Country;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Caching.Memory;
+    using Services.Company;
+    using Services.Country;
+    using Services.Internship;
+
+    using static Common.WebConstants;
+
+    public class InternshipController : CompanyController
+    {
+        private readonly IInternshipService internshipService;
+        private readonly ICompanyService companyService;
+        private readonly ICountryService countryService;
+        private readonly IMemoryCache cache;
+
+        public InternshipController(
+            IInternshipService internshipService,
+            ICompanyService companyService,
+            ICountryService countryService,
+            IMemoryCache cache)
+        {
+            this.internshipService = internshipService;
+            this.companyService = companyService;
+            this.countryService = countryService;
+            this.cache = cache;
+        }
+
+        public IActionResult MyInternships([FromQuery] InternshipListingQueryModel queryModel)
+        {
+            if (!companyService.IsCompanyByUserId(User.Id()))
+            {
+                return Unauthorized();
+            }
+
+            var companyId = companyService.GetIdByUserId(User.Id());
+
+            queryModel = internshipService.All(
+                queryModel.Role,
+                queryModel.IsPaid,
+                queryModel.IsRemote,
+                companyId,
+                queryModel.CountryId,
+                queryModel.CurrentPage,
+                queryModel.InternshipsPerPage);
+
+            queryModel.Countries = AcquireCachedCountries();
+
+            return View(queryModel);
+        }
+
+        public IActionResult Add()
+        {
+            if (!companyService.IsCompanyByUserId(User.Id()))
+            {
+                return Unauthorized();
+            }
+
+            var internshipModel = new AddInternshipFormModel
+            {
+                CompanyId = companyService.GetIdByUserId(User.Id()),
+                Countries = AcquireCachedCountries()
+            };
+
+            return View(internshipModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddInternshipFormModel internship)
+        {
+            if (!companyService.IsCompanyByUserId(User.Id()))
+            {
+                return Unauthorized();
+            }
+
+            if (!companyService.Exists(internship.CompanyId))
+            {
+                ModelState.AddModelError(nameof(internship.CompanyId), "Invalid option.");
+            }
+
+            if (internship.CountryId != null)
+            {
+                if (!countryService.Exists(internship.CountryId))
+                {
+                    ModelState.AddModelError(nameof(internship.CountryId), "Invalid option.");
+                }
+            }
+
+            if (internship.IsPaid && internship.SalaryUSD == null)
+            {
+                ModelState.AddModelError(nameof(internship.SalaryUSD), "SalaryUSD is mandatory when internship is paid.");
+            }
+
+            if (!internship.IsPaid && internship.SalaryUSD != null)
+            {
+                ModelState.AddModelError(nameof(internship.SalaryUSD), "SalaryUSD is not required when internship is not paid.");
+            }
+
+            if (internship.IsRemote && internship.CountryId != null)
+            {
+                ModelState.AddModelError(nameof(internship.CountryId), "Remote internships do not require a country.");
+            }
+
+            if (!internship.IsRemote && internship.CountryId == null)
+            {
+                ModelState.AddModelError(nameof(internship.CountryId), "Non-remote internships require a country.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                internship.Countries = AcquireCachedCountries();
+
+                return View(internship);
+            }
+
+            var internshipId = internshipService.Add(
+                internship.CompanyId,
+                internship.Role,
+                internship.IsPaid,
+                internship.SalaryUSD,
+                internship.IsRemote,
+                internship.Description,
+                internship.CountryId);
+
+            TempData[GlobalMessageKey] = "Internship published successfully.";
+
+            return RedirectToAction("Details", "Internship", new { id = internshipId });
+        }
+
+        public IActionResult Edit(string id)
+        {
+            if (!IsCurrentCompanyOwner(id))
+            {
+                return Unauthorized();
+            }
+
+            var internship = internshipService.GetEditModel(id);
+
+            if (internship == null)
+            {
+                return NotFound();
+            }
+
+            return View(internship);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EditInternshipFormModel internship)
+        {
+            if (!IsCurrentCompanyOwner(internship.Id))
+            {
+                return Unauthorized();
+            }
+
+            if (internship.IsPaid && internship.SalaryUSD == null)
+            {
+                ModelState.AddModelError(nameof(internship.SalaryUSD), "SalaryUSD is mandatory when internship is paid.");
+            }
+
+            if (!internship.IsPaid && internship.SalaryUSD != null)
+            {
+                internship.SalaryUSD = null;
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(internship);
+            }
+
+            var editResult = internshipService.Edit(
+                internship.Id,
+                internship.IsPaid,
+                internship.SalaryUSD,
+                internship.Description);
+
+            if (!editResult)
+            {
+                return BadRequest();
+            }
+
+            TempData[GlobalMessageKey] = "Successfully edited internship.";
+
+            return RedirectToAction("Details", "Internship", new { id = internship.Id });
+        }
+
+        public IActionResult Delete(string id)
+        {
+            if (!IsCurrentCompanyOwner(id))
+            {
+                return Unauthorized();
+            }
+
+            var deleteResult = internshipService.Delete(id);
+
+            if (!deleteResult)
+            {
+                return BadRequest();
+            }
+
+            TempData[GlobalMessageKey] = "Successfully deleted internship.";
+
+            var companyId = companyService.GetIdByUserId(User.Id());
+
+            return RedirectToAction(nameof(MyInternships), new { companyId = companyId });
+        }
+
+        private bool IsCurrentCompanyOwner(string internshipId)
+            => internshipService
+                .IsInternshipOwnedByCompany(
+                internshipId,
+                companyService.GetIdByUserId(User.Id()));
+
+        private IEnumerable<CountryListingViewModel> AcquireCachedCountries()
+        {
+            var countries = cache.Get<IEnumerable<CountryListingViewModel>>(CountriesCacheKey);
+
+            if (countries == null)
+            {
+                countries = countryService.All();
+
+                var cacheOptions = new MemoryCacheEntryOptions()
+                    .SetPriority(CacheItemPriority.NeverRemove);
+
+                cache.Set(CountriesCacheKey, countries, cacheOptions);
+            }
+
+            return countries;
+        }
+
+    }
+}
--- Internify/Web/Internify.Web/Controllers/InternshipController.cs
+++ Internify/Web/Internify.Web/Controllers/InternshipController.cs
@@ -1,9 +1,7 @@
 ﻿namespace Internify.Web.Controllers
 {
-    using Infrastructure.Extensions;
     using Internify.Models.InputModels.Internship;
     using Internify.Models.ViewModels.Country;
-    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Caching.Memory;
     using Services.Company;
@@ -48,168 +46,6 @@
             return View(queryModel);
         }
 
-        [Authorize]
-        public IActionResult MyInternships([FromQuery] InternshipListingQueryModel queryModel)
-        {
-            if (!companyService.IsCompanyByUserId(User.Id()))
-            {
-                return Unauthorized();
-            }
-
-            var companyId = companyService.GetIdByUserId(User.Id());
-
-            queryModel = internshipService.All(
-                queryModel.Role,
-                queryModel.IsPaid,
-                queryModel.IsRemote,
-                companyId,
-                queryModel.CountryId,
-                queryModel.CurrentPage,
-                queryModel.InternshipsPerPage);
-
-            queryModel.Countries = AcquireCachedCountries();
-
-            return View(queryModel);
-        }
-
-        [Authorize]
-        public IActionResult Add()
-        {
-            if (!companyService.IsCompanyByUserId(User.Id()))
-            {
-                return Unauthorized();
-            }
-
-            var internshipModel = new AddInternshipFormModel
-            {
-                CompanyId = companyService.GetIdByUserId(User.Id()),
-                Countries = AcquireCachedCountries()
-            };
-
-            return View(internshipModel);
-        }
-
-        [Authorize]
-        [HttpPost]
-        public IActionResult Add(AddInternshipFormModel internship)
-        {
-            if (!companyService.IsCompanyByUserId(User.Id()))
-            {
-                return Unauthorized();
-            }
-
-            if (!companyService.Exists(internship.CompanyId))
-            {
-                ModelState.AddModelError(nameof(internship.CompanyId), "Invalid option.");
-            }
-
-            if (internship.CountryId != null)
-            {
-                if (!countryService.Exists(internship.CountryId))
-                {
-                    ModelState.AddModelError(nameof(internship.CountryId), "Invalid option.");
-                }
-            }
-
-            if (internship.IsPaid && internship.SalaryUSD == null)
-            {
-                ModelState.AddModelError(nameof(internship.SalaryUSD), "SalaryUSD is mandatory when internship is paid.");
-            }
-
-            if (!internship.IsPaid && internship.SalaryUSD != null)
-            {
-                ModelState.AddModelError(nameof(internship.SalaryUSD), "SalaryUSD is not required when internship is not paid.");
-            }
-
-            if (internship.IsRemote && internship.CountryId != null)
-            {
-                ModelState.AddModelError(nameof(internship.CountryId), "Remote internships do not require a country.");
-            }
-
-            if (!internship.IsRemote && internship.CountryId == null)
-            {
-                ModelState.AddModelError(nameof(internship.CountryId), "Non-remote internships require a country.");
-            }
-
-            if (!ModelState.IsValid)
-            {
-                internship.Countries = AcquireCachedCountries();
-
-                return View(internship);
-            }
-
-            var internshipId = internshipService.Add(
-                internship.CompanyId,
-                internship.Role,
-                internship.IsPaid,
-                internship.SalaryUSD,
-                internship.IsRemote,
-                internship.Description,
-                internship.CountryId);
-
-            TempData[GlobalMessageKey] = "Internship published successfully.";
-
-            return RedirectToAction(nameof(Details), new { id = internshipId });
-        }
-
-        [Authorize]
-        public IActionResult Edit(string id)
-        {
-            if (!IsCurrentCompanyOwner(id))
-            {
-                return Unauthorized();
-            }
-
-            var internship = internshipService.GetEditModel(id);
-
-            if (internship == null)
-            {
-                return NotFound();
-            }
-
-            return View(internship);
-        }
-
-        [Authorize]
-        [HttpPost]
-        public IActionResult Edit(EditInternshipFormModel internship)
-        {
-            if (!IsCurrentCompanyOwner(internship.Id))
-            {
-                return Unauthorized();
-            }
-
-            if (internship.IsPaid && internship.SalaryUSD == null)
-            {
-                ModelState.AddModelError(nameof(internship.SalaryUSD), "SalaryUSD is mandatory when internship is paid.");
-            }
-
-            if (!internship.IsPaid && internship.SalaryUSD != null)
-            {
-                internship.SalaryUSD = null;
-            }
-
-            if (!ModelState.IsValid)
-            {
-                return View(internship);
-            }
-
-            var editResult = internshipService.Edit(
-                internship.Id,
-                internship.IsPaid,
-                internship.SalaryUSD,
-                internship.Description);
-
-            if (!editResult)
-            {
-                return BadRequest();
-            }
-
-            TempData[GlobalMessageKey] = "Successfully edited internship.";
-
-            return RedirectToAction(nameof(Details), new { id = internship.Id });
-        }
-
         public IActionResult Details(string id)
         {
             var internship = internshipService.GetDetailsModel(id);
@@ -221,32 +57,6 @@
 
             return View(internship);
         }
-
-        [Authorize]
-        public IActionResult Delete(string id)
-        {
-            if (!IsCurrentCompanyOwner(id))
-            {
-                return Unauthorized();
-            }
-
-            var deleteResult = internshipService.Delete(id);
-
-            if (!deleteResult)
-            {
-                return BadRequest();
-            }
-
-            TempData[GlobalMessageKey] = "Successfully deleted internship.";
-
-            return RedirectToAction(nameof(All));
-        }
-
-        private bool IsCurrentCompanyOwner(string internshipId)
-            => internshipService
-                .IsInternshipOwnedByCompany(
-                internshipId,
-                companyService.GetIdByUserId(User.Id()));
 
         private IEnumerable<CountryListingViewModel> AcquireCachedCountries()
         {
