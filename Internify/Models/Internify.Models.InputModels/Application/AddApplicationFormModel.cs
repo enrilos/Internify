0055e3a4e8e5611776427692f2aceceb38e6e2c0
emrrar <avatareigan@gmail.com>
2022-03-12 19:07:09 +0200

    Added AddApplicationFormModel

---
+++ Internify/Models/Internify.Models.InputModels/Application/AddApplicationFormModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Internify.Models.InputModels.Application
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using static Data.Common.DataConstants.Application;
+
+    public class AddApplicationFormModel
+    {
+        [Required]
+        public string InternshipId { get; set; }
+
+        [Required]
+        public string CandidateId { get; set; }
+
+        [Required]
+        [MaxLength(CoverLetterMaxLength)]
+        [Display(Name = "Cover Letter")]
+        public string CoverLetter { get; set; }
+    }
+}
