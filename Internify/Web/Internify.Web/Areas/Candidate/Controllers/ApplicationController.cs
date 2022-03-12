4f8baabde15b39b0261894dea9495796891acaf0
emrrar <avatareigan@gmail.com>
2022-03-12 19:10:03 +0200

    Candidates can submit applications for particular internships.

--- Internify/Web/Internify.Web/Areas/Candidate/Controllers/ApplicationController.cs
+++ Internify/Web/Internify.Web/Areas/Candidate/Controllers/ApplicationController.cs
@@ -1,6 +1,81 @@
 ﻿namespace Internify.Web.Areas.Candidate.Controllers
 {
+    using Infrastructure.Extensions;
+    using Internify.Models.InputModels.Application;
+    using Microsoft.AspNetCore.Mvc;
+    using Services.Application;
+    using Services.Candidate;
+    using Services.Internship;
+
     public class ApplicationController : CandidateController
     {
+        private readonly IApplicationService applicationService;
+        private readonly IInternshipService internshipService;
+        private readonly ICandidateService candidateService;
+
+        public ApplicationController(
+            IApplicationService applicationService,
+            IInternshipService internshipService,
+            ICandidateService candidateService)
+        {
+            this.applicationService = applicationService;
+            this.internshipService = internshipService;
+            this.candidateService = candidateService;
+        }
+
+        public IActionResult MyApplications(string candidateId)
+        {
+            // applications listing and other data;
+
+            // TODO...
+
+            var obj = new { };
+
+            return View(obj);
+        }
+
+        public IActionResult ApplyForInternship(string id)
+        {
+            var candidateId = candidateService.GetIdByUserId(User.Id());
+
+            var applicationModel = new AddApplicationFormModel
+            {
+                InternshipId = id,
+                CandidateId = candidateId
+            };
+
+            return View(applicationModel);
+        }
+
+        [HttpPost]
+        public IActionResult ApplyForInternship(AddApplicationFormModel application)
+        {
+            if (!internshipService.Exists(application.InternshipId))
+            {
+                ModelState.AddModelError(nameof(application.InternshipId), "Invalid option.");
+            }
+
+            if (!candidateService.Exists(application.CandidateId))
+            {
+                ModelState.AddModelError(nameof(application.CandidateId), "Invalid option.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var result = applicationService.Add(
+                application.InternshipId,
+                application.CandidateId,
+                application.CoverLetter);
+
+            if (!result)
+            {
+                return BadRequest();
+            }
+
+            return RedirectToAction("Details", "Internship", new { id = application.InternshipId });
+        }
     }
 }
