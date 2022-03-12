389d67e9af29af98819e60f9ef11deeb2589fb32
emrrar <avatareigan@gmail.com>
2022-03-12 19:05:37 +0200

    Added application service.

---
+++ Internify/Services/Internify.Services/Application/ApplicationService.cs
@@ -0,0 +1,47 @@
+﻿namespace Internify.Services.Application
+{
+    using Data;
+    using Data.Models;
+
+    public class ApplicationService : IApplicationService
+    {
+        private readonly InternifyDbContext data;
+
+        public ApplicationService(InternifyDbContext data)
+            => this.data = data;
+
+        public bool Add(
+            string internshipId,
+            string candidateId,
+            string coverLetter)
+        {
+            var application = new Application
+            {
+                InternshipId = internshipId,
+                CandidateId = candidateId,
+                CoverLetter = coverLetter
+            };
+
+            data.Applications.Add(application);
+
+            var result = data.SaveChanges();
+
+            if (result == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool HasCandidateApplied(
+            string candidateId,
+            string internshipId)
+            => data
+            .Applications
+            .Any(x =>
+            x.CandidateId == candidateId
+            && x.InternshipId == internshipId
+            && !x.IsDeleted);
+    }
+}
---
+++ Internify/Services/Internify.Services/Application/IApplicationService.cs
@@ -0,0 +1,14 @@
+﻿namespace Internify.Services.Application
+{
+    public interface IApplicationService
+    {
+        public bool Add(
+            string internshipId,
+            string candidateId,
+            string coverLetter);
+
+        public bool HasCandidateApplied(
+            string candidateId,
+            string internshipId);
+    }
+}
