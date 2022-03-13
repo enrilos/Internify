d197ac2ca3f1ac42262e9e2c5c8ab04492ea1bab
emrrar <avatareigan@gmail.com>
2022-03-13 15:59:04 +0200

    Added MyApplicationListingQueryModel

---
+++ Internify/Models/Internify.Models.InputModels/Application/MyApplicationListingQueryModel.cs
@@ -0,0 +1,27 @@
+﻿namespace Internify.Models.InputModels.Application
+{
+    using System.ComponentModel.DataAnnotations;
+    using ViewModels.Application;
+    using ViewModels.Company;
+
+    public class MyApplicationListingQueryModel
+    {
+        public string CandidateId { get; set; }
+
+        public string Role { get; set; }
+
+        [Display(Name = "Company")]
+        public string CompanyId { get; set; }
+
+        public IEnumerable<CompanySelectOptionsViewModel> Companies { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        [Display(Name = "Per Page")]
+        public int ApplicationsPerPage { get; set; }
+
+        public IEnumerable<MyApplicationListingViewModel> Applications { get; set; }
+
+        public int TotalApplications { get; set; }
+    }
+}
