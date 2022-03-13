bb02b956c798b0af3b7954d8abcffc1a27cf05ec
emrrar <avatareigan@gmail.com>
2022-03-13 18:56:16 +0200

    Added ApplicationDetailsViewModel

---
+++ Internify/Models/Internify.Models.ViewModels/Application/ApplicationDetailsViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Internify.Models.ViewModels.Application
+{
+    public class ApplicationDetailsViewModel
+    {
+        public string Id { get; init; }
+
+        public string Role { get; init; }
+        
+        public string InternshipId { get; init; }
+
+        public string Company { get; init; }
+
+        public string CompanyId { get; init; }
+
+        public string CoverLetter { get; init; }
+
+        public DateTime CreatedOn { get; init; }
+
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
