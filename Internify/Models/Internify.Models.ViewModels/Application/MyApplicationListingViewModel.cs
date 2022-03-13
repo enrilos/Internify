9b30b09a57fa257e1b2f8b6c82812e873f587aa5
emrrar <avatareigan@gmail.com>
2022-03-13 15:59:24 +0200

    Added MyApplicationListingViewModel

---
+++ Internify/Models/Internify.Models.ViewModels/Application/MyApplicationListingViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Internify.Models.ViewModels.Application
+{
+    public class MyApplicationListingViewModel
+    {
+        public string Id { get; set; }
+
+        public string Role { get; set; }
+
+        public string Company { get; set; }
+    }
+}
