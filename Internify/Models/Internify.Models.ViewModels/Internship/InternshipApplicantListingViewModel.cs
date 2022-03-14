2eb2273c8b2eea4cfd73e7aceda03b26ec22f29f
emrrar <avatareigan@gmail.com>
2022-03-14 18:35:10 +0200

    Added InternshipApplicantListingViewModel

---
+++ Internify/Models/Internify.Models.ViewModels/Internship/InternshipApplicantListingViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Internify.Models.ViewModels.Internship
+{
+    public class InternshipApplicantListingViewModel
+    {
+        public string ApplicationId { get; init; }
+
+        public string CandidateImageUrl { get; init; }
+
+        public string CandidateFullName { get; init; }
+
+        public int CandidateAge { get; init; }
+
+        public string CandidateCoverLetterSegment { get; init; }
+    }
+}
