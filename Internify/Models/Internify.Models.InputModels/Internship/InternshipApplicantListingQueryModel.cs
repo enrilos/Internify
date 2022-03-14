d3098b414b5fbabfcccc9837d73579b877d543d2
emrrar <avatareigan@gmail.com>
2022-03-14 18:35:28 +0200

    Added InternshipApplicantListingQueryModel

---
+++ Internify/Models/Internify.Models.InputModels/Internship/InternshipApplicantListingQueryModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Internify.Models.InputModels.Internship
+{
+    using ViewModels.Internship;
+    using System.ComponentModel.DataAnnotations;
+
+    public class InternshipApplicantListingQueryModel
+    {
+        public string InternshipId { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        [Display(Name = "Per Page")]
+        public int ApplicantsPerPage { get; set; }
+
+        public IEnumerable<InternshipApplicantListingViewModel> Applicants { get; set; }
+
+        public int TotalApplicants { get; set; }
+    }
+}
