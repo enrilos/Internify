797c1720ce452ee772aa63baba1f03f9fe7f32d0
emrrar <avatareigan@gmail.com>
2022-03-15 21:59:22 +0200

    Added AddArticleFormModel

---
+++ Internify/Models/Internify.Models.InputModels/Article/AddArticleFormModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Internify.Models.InputModels.Article
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using static Data.Common.DataConstants;
+    using static Data.Common.DataConstants.Article;
+
+    public class AddArticleFormModel
+    {
+        [Required]
+        public string CompanyId { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: TitleMaxLength, MinimumLength = TitleMinLength)]
+        public string Title { get; set; }
+
+        [MaxLength(UrlMaxLength)]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: ContentMaxLength, MinimumLength = ContentMinLength)]
+        public string Content { get; set; }
+    }
+}
