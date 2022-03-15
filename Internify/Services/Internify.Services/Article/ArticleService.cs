af776e3305d1c0b308de282997a38c3edb878d8d
emrrar <avatareigan@gmail.com>
2022-03-15 22:00:39 +0200

    Implemented the ability to add articles in the corresponding service.

---
+++ Internify/Services/Internify.Services/Article/ArticleService.cs
@@ -0,0 +1,35 @@
+﻿namespace Internify.Services.Article
+{
+    using Data;
+    using Data.Models;
+
+    public class ArticleService : IArticleService
+    {
+        private readonly InternifyDbContext data;
+
+        public ArticleService(InternifyDbContext data)
+            => this.data = data;
+
+        public string Add(
+            string companyId,
+            string title,
+            string imageUrl,
+            string content,
+            string hostName)
+        {
+            var article = new Article
+            {
+                Title = title.Trim(),
+                ImageUrl = string.IsNullOrEmpty(imageUrl) ? Path.Combine(hostName, "/images/article.jpg") : imageUrl.Trim(),
+                Content = content.Trim(),
+                CompanyId = companyId
+            };
+
+            data.Articles.Add(article);
+
+            data.SaveChanges();
+
+            return article.Id;
+        }
+    }
+}
---
+++ Internify/Services/Internify.Services/Article/IArticleService.cs
@@ -0,0 +1,12 @@
+﻿namespace Internify.Services.Article
+{
+    public interface IArticleService
+    {
+        public string Add(
+            string companyId,
+            string title,
+            string imageUrl,
+            string content,
+            string hostName);
+    }
+}
