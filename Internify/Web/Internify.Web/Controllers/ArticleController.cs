3b1fd935491cd150f8e51c9abd14249c98d4ab7a
emrrar <avatareigan@gmail.com>
2022-03-15 22:10:35 +0200

    Updated the company Details page. Articles are visible for everyone.

---
+++ Internify/Web/Internify.Web/Controllers/ArticleController.cs
@@ -0,0 +1,27 @@
+﻿namespace Internify.Web.Controllers
+{
+    using Internify.Models.InputModels.Article;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ArticleController : Controller
+    {
+        // services..
+
+        // ctor..
+
+        public IActionResult CompanyArticles([FromQuery] ArticleListingQueryModel queryModel)
+        {
+            // TODO..
+            // filter/paginate..
+
+            return null;
+        }
+
+        public IActionResult Details(string id)
+        {
+            // check if company is owner.
+
+            return View(new Object { });
+        }
+    }
+}
