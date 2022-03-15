0d7915d8d9454cf407f2c9249c2cf23205b45779
emrrar <avatareigan@gmail.com>
2022-03-15 16:51:46 +0200

    Added InternListingViewModel

---
+++ Internify/Models/Internify.Models.ViewModels/Intern/InternListingViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Internify.Models.ViewModels.Intern
+{
+    public class InternListingViewModel
+    {
+        public string Id { get; init; }
+
+        public string FullName { get; init; }
+
+        public string ImageUrl { get; init; }
+
+        public int Age { get; init; }
+
+        public string InternshipRole { get; init; }
+    }
+}
