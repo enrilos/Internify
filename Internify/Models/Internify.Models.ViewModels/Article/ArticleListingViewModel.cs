a2d36572cca3f4f3163dc4f433b7c0c9cda16d09
emrrar <avatareigan@gmail.com>
2022-03-16 15:24:26 +0200

    Added ArticleListingViewModel

---
+++ Internify/Models/Internify.Models.ViewModels/Article/ArticleListingViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Internify.Models.ViewModels.Article
+{
+    public class ArticleListingViewModel
+    {
+        public string Id { get; init; }
+
+        public string Title { get; init; }
+
+        public string ImageUrl { get; init; }
+    }
+}
