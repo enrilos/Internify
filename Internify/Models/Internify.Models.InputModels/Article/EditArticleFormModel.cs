8443f4238877d18ee140de98f34a8f95d130871e
emrrar <avatareigan@gmail.com>
2022-03-16 18:49:24 +0200

    Added EditArticleFormModel

---
+++ Internify/Models/Internify.Models.InputModels/Article/EditArticleFormModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Internify.Models.InputModels.Article
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using static Data.Common.DataConstants.Article;
+
+    public class EditArticleFormModel
+    {
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: TitleMaxLength, MinimumLength = TitleMinLength)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: ContentMaxLength, MinimumLength = ContentMinLength)]
+        public string Content { get; set; }
+    }
+}
