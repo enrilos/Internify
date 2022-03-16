3b9a6e02b69e9b9c827dcf5067ebd39ecaa8f747
emrrar <avatareigan@gmail.com>
2022-03-16 15:25:11 +0200

    Added ArticleListingQueryModel

---
+++ Internify/Models/Internify.Models.InputModels/Article/ArticleListingQueryModel.cs
@@ -0,0 +1,26 @@
+﻿namespace Internify.Models.InputModels.Article
+{
+    using System.ComponentModel.DataAnnotations;
+    using ViewModels.Article;
+
+    using static Data.Common.DataConstants.Article;
+
+    public class ArticleListingQueryModel
+    {
+        public string CompanyId { get; set; }
+
+        public string CompanyName { get; set; }
+
+        [StringLength(maximumLength: TitleMaxLength, MinimumLength = TitleMinLength)]
+        public string Title { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        [Display(Name = "Per Page")]
+        public int ArticlesPerPage { get; set; }
+
+        public IEnumerable<ArticleListingViewModel> Articles { get; set; }
+
+        public int TotalArticles { get; set; }
+    }
+}
