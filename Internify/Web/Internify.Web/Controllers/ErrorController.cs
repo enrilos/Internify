c7efa5598ef744c847937ba9a3509a99957ec42f
emrrar <avatareigan@gmail.com>
2022-03-17 22:46:09 +0200

    Added ErrorController plus HTTP 400, 401 and 404 view pages.

---
+++ Internify/Web/Internify.Web/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿namespace Internify.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ErrorController : Controller
+    {
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            switch (statusCode)
+            {
+                case 400: return View("BadRequest");
+                case 401: return View("Unauthorized");
+                case 404: return View("NotFound");
+                default: return RedirectToAction("Error", "Home");
+            }
+        }
+    }
+}
