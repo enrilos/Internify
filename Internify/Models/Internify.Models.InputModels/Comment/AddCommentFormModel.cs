120daaf566630de616b3ef8d5e8b85c20238d793
emrrar <avatareigan@gmail.com>
2022-03-18 18:41:08 +0200

    Added AddCommentFormModel

---
+++ Internify/Models/Internify.Models.InputModels/Comment/AddCommentFormModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Internify.Models.InputModels.Comment
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using static Data.Common.DataConstants.Comment;
+
+    public class AddCommentFormModel
+    {
+        [Required]
+        public string ArticleId { get; set; }
+
+        [Required]
+        public string CandidateId { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: ContentMaxLength, MinimumLength = ContentMinLength)]
+        public string Content { get; set; }
+    }
+}
