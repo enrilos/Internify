f9c19b9d99eb86f8c5f824932a7298184577c9bd
emrrar <avatareigan@gmail.com>
2022-03-19 19:31:23 +0200

    Added AddReviewFormModel

---
+++ Internify/Models/Internify.Models.InputModels/Review/AddReviewFormModel.cs
@@ -0,0 +1,26 @@
+﻿namespace Internify.Models.InputModels.Review
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using static Data.Common.DataConstants.Review;
+
+    public class AddReviewFormModel
+    {
+        [Required]
+        public string CandidateId { get; set; }
+
+        [Required]
+        public string CompanyId { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: TitleMaxLength, MinimumLength = TitleMinLength)]
+        public string Title { get; set; }
+
+        [Range(minimum: 1, maximum: 5)]
+        public int Rating { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: ContentMaxLength, MinimumLength = ContentMinLength)]
+        public string Content { get; set; }
+    }
+}
