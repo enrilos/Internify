533c734024bc4f40958a43abce6ced0190f9cf78
emrrar <avatareigan@gmail.com>
2022-03-19 18:52:46 +0200

    Article's comments listing with pagination works.

---
+++ Internify/Web/Internify.Web/Controllers/CommentController.cs
@@ -0,0 +1,36 @@
+﻿namespace Internify.Web.Controllers
+{
+    using Internify.Models.InputModels.Comment;
+    using Microsoft.AspNetCore.Mvc;
+    using Services.Article;
+    using Services.Comment;
+
+    public class CommentController : Controller
+    {
+        private readonly ICommentService commentService;
+        private readonly IArticleService articleService;
+
+        public CommentController(
+            ICommentService commentService,
+            IArticleService articleService)
+        {
+            this.commentService = commentService;
+            this.articleService = articleService;
+        }
+
+        public IActionResult ArticleComments([FromQuery] CommentListingQueryModel queryModel)
+        {
+            if (!articleService.Exists(queryModel.ArticleId))
+            {
+                return NotFound();
+            }
+
+            queryModel = commentService.ArticleComments(
+                queryModel.ArticleId,
+                queryModel.CurrentPage,
+                queryModel.CommentsPerPage);
+
+            return View(queryModel);
+        }
+    }
+}
