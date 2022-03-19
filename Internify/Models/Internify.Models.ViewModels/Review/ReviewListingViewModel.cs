5661a87af71c8fdf979ba070b9b682304db32e5b
emrrar <avatareigan@gmail.com>
2022-03-19 21:04:51 +0200

    Added ReviewListingViewModel

---
+++ Internify/Models/Internify.Models.ViewModels/Review/ReviewListingViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Internify.Models.ViewModels.Review
+{
+    public class ReviewListingViewModel
+    {
+        public string Id { get; init; }
+
+        public string CandidateFullName { get; init; }
+
+        public string CandidateId { get; init; }
+
+        public string Title { get; init; }
+
+        public int Rating { get; init; }
+
+        public string Content { get; init; }
+
+        public DateTime CreatedOn { get; init; }
+
+        public DateTime? ModifiedOn { get; init; }
+    }
+}
