c93e0bcf08f92813458fadb394ed21019502a391
emrrar <avatareigan@gmail.com>
2022-03-19 21:07:47 +0200

    Anyone can access a company's reviews.

---
+++ Internify/Web/Internify.Web/Controllers/ReviewController.cs
@@ -0,0 +1,38 @@
+﻿namespace Internify.Web.Controllers
+{
+    using Internify.Models.InputModels.Review;
+    using Microsoft.AspNetCore.Mvc;
+    using Services.Company;
+    using Services.Review;
+
+    public class ReviewController : Controller
+    {
+        private readonly IReviewService reviewService;
+        private readonly ICompanyService companyService;
+
+        public ReviewController(
+            IReviewService reviewService,
+            ICompanyService companyService)
+        {
+            this.reviewService = reviewService;
+            this.companyService = companyService;
+        }
+
+        public IActionResult CompanyReviews(ReviewListingQueryModel queryModel)
+        {
+            if (!companyService.Exists(queryModel.CompanyId))
+            {
+                return NotFound();
+            }
+
+            queryModel = reviewService.CompanyReviews(
+                queryModel.CompanyId,
+                queryModel.Title,
+                queryModel.Rating,
+                queryModel.CurrentPage,
+                queryModel.ReviewsPerPage);
+
+            return View(queryModel);
+        }
+    }
+}
