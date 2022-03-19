ca5922693ba8682a08c2e3dc80c9c226e6c8cb52
emrrar <avatareigan@gmail.com>
2022-03-19 18:48:58 +0200

    Added CommentListingQueryModel

---
+++ Internify/Models/Internify.Models.InputModels/Comment/CommentListingQueryModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Internify.Models.InputModels.Comment
+{
+    using Models.ViewModels.Comment;
+    using System.ComponentModel.DataAnnotations;
+
+    public class CommentListingQueryModel
+    {
+        public string ArticleId { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        [Display(Name = "Per Page")]
+        public int CommentsPerPage { get; set; }
+
+        public IEnumerable<CommentListingViewModel> Comments { get; set; }
+
+        public int TotalComments { get; set; }
+    }
+}
