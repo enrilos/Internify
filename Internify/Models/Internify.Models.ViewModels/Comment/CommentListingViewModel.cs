7302d9c73d095fe0697c1399b05f12b2218a9596
emrrar <avatareigan@gmail.com>
2022-03-19 18:49:17 +0200

    Added CommentListingViewModel

---
+++ Internify/Models/Internify.Models.ViewModels/Comment/CommentListingViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Internify.Models.ViewModels.Comment
+{
+    public class CommentListingViewModel
+    {
+        public string CandidateFullName { get; init; }
+
+        public string CandidateId { get; init; }
+
+        public string Content { get; init; }
+
+        public DateTime CreatedOn { get; init; }
+    }
+}
