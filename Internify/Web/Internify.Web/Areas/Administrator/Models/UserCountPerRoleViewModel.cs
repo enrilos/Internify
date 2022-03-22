561e626738bc410433c49b96886c230c68f9869a
emrrar <avatareigan@gmail.com>
2022-03-22 14:50:09 +0200

    Added UserCountPerRoleViewModel

---
+++ Internify/Web/Internify.Web/Areas/Administrator/Models/UserCountPerRoleViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Internify.Web.Areas.Administrator.Models
+{
+    public class UserCountPerRoleViewModel
+    {
+        public int Candidates { get; init; }
+
+        public int Companies { get; init; }
+
+        public int Universities { get; init; }
+    }
+}
