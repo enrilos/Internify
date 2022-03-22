208d7e06b31be7377cc18900e9fd0284e06e2bb7
emrrar <avatareigan@gmail.com>
2022-03-22 14:52:40 +0200

    Administrator can view a data chart in Dashboard page with the total number of candidates/companies/universities.

--- Internify/Web/Internify.Web/Areas/Administrator/Controllers/AdministratorController.cs
+++ Internify/Web/Internify.Web/Areas/Administrator/Controllers/AdministratorController.cs
@@ -1,14 +1,41 @@
 ﻿namespace Internify.Web.Areas.Administrator.Controllers
 {
-    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
+    using Models;
+    using Services.Candidate;
+    using Services.Company;
+    using Services.University;
 
-    using static Internify.Common.GlobalConstants;
-
-    [Area(AdministratorRoleName)]
-    [Authorize(Roles = AdministratorRoleName)]
-    [Route($"{AdministratorRoleName}/[controller]/[action]/{{id?}}")]
-    public class AdministratorController : Controller
+    public class AdministratorController : AdministratorControllerBase
     {
+        private readonly ICandidateService candidateService;
+        private readonly ICompanyService companyService;
+        private readonly IUniversityService universityService;
+
+        public AdministratorController(
+            ICandidateService candidateService,
+            ICompanyService companyService,
+            IUniversityService universityService)
+        {
+            this.candidateService = candidateService;
+            this.companyService = companyService;
+            this.universityService = universityService;
+        }
+
+        public IActionResult Dashboard()
+        {
+            // make dashboard  btn dropdown? with options:
+            // users (all)
+            // internships (all)
+
+            var data = new UserCountPerRoleViewModel
+            {
+                Candidates = candidateService.TotalCount(),
+                Companies = companyService.TotalCount(),
+                Universities = universityService.TotalCount(),
+            };
+
+            return View(data);
+        }
     }
 }
