917de792fd8c0c07d38bfbc4493c5f87ea822760
emrrar <avatareigan@gmail.com>
2022-03-26 19:36:00 +0200

    Finished ApplicationServiceTests.

--- Internify/Tests/Internify.Tests/Services/ApplicationServiceTests.cs
+++ Internify/Tests/Internify.Tests/Services/ApplicationServiceTests.cs
@@ -4,15 +4,19 @@
     using Data.Models;
     using Internify.Services.Application;
     using Microsoft.EntityFrameworkCore;
+    using Models.InputModels.Application;
     using Models.ViewModels.Application;
     using NUnit.Framework;
     using System;
+    using System.Linq;
 
     public class ApplicationServiceTests
     {
         private InternifyDbContext data;
         private IApplicationService applicationService;
 
+        private string specializationId;
+        private string countryId;
         private string internshipId;
         private string companyId;
         private string candidateId;
@@ -76,7 +80,7 @@
 
             Assert.IsNull(actualResult);
         }
-        
+
         [Test]
         public void Edit_ShouldChange_ModifiedOnProperty_AndReturn_ApplicationIdWhenSuccessful()
         {
@@ -428,7 +432,6 @@
               candidateId,
               "This is cover letter This is a cover letter This is a cover letter");
 
-            var actualModel = applicationService.GetDetailsModel(id);
             var expectedModel = new ApplicationDetailsViewModel
             {
                 Id = id,
@@ -438,6 +441,8 @@
                 CompanyId = companyId,
                 CoverLetter = "This is cover letter This is a cover letter This is a cover letter"
             };
+
+            var actualModel = applicationService.GetDetailsModel(id);
 
             Assert.That(actualModel, Has.Property("Id").EqualTo(expectedModel.Id)
                                    & Has.Property("Role").EqualTo(expectedModel.Role)
@@ -452,19 +457,405 @@
         [Test]
         public void GetDetailsModelForCompany_ShouldReturn_NullWhenApplicationIdDoesNotMatch()
         {
+            var actualResult = applicationService.GetDetailsModelForCompany("");
 
+            Assert.IsNull(actualResult);
         }
 
         [Test]
         public void GetDetailsModelForCompany_ShouldReturn_NullWhenApplicationIsDeleted()
         {
+            SeedDatabase();
 
+            var id = applicationService.Add(
+              internshipId,
+              candidateId,
+              "This is cover letter This is a cover letter This is a cover letter");
+
+            applicationService.Delete(id);
+
+            var actualResult = applicationService.GetDetailsModelForCompany(id);
+
+            Assert.IsNull(actualResult);
         }
 
         [Test]
         public void GetDetailsModelForCompany_ShouldReturn_ApplicationForCompanyDetailsViewModel()
         {
+            SeedDatabase();
 
+            var id = applicationService.Add(
+              internshipId,
+              candidateId,
+              "This is cover letter This is a cover letter This is a cover letter");
+
+            var expectedModel = new ApplicationForCompanyDetailsViewModel
+            {
+                InternshipId = internshipId,
+                InternshipRole = "Junior Full-Stack Developer",
+                CandidateId = candidateId,
+                CandidateFullName = "Emre Ereceb",
+                CandidateImageUrl = null,
+                CandidateCoverLetter = "This is cover letter This is a cover letter This is a cover letter"
+            };
+
+            var actualModel = applicationService.GetDetailsModelForCompany(id);
+
+            Assert.That(actualModel, Has.Property("InternshipId").EqualTo(expectedModel.InternshipId)
+                                   & Has.Property("InternshipRole").EqualTo(expectedModel.InternshipRole)
+                                   & Has.Property("CandidateId").EqualTo(expectedModel.CandidateId)
+                                   & Has.Property("CandidateFullName").EqualTo(expectedModel.CandidateFullName)
+                                   & Has.Property("CandidateCoverLetter").EqualTo(expectedModel.CandidateCoverLetter));
+
+            Assert.IsNull(actualModel.CandidateImageUrl);
+            Assert.That(actualModel.CandidateAge, Is.GreaterThanOrEqualTo(16));
+        }
+
+        [Test]
+        public void GetEditModelForCompany_ShouldReturn_NullWhenApplicationIdDoesNotMatch()
+        {
+            var actualResult = applicationService.GetEditModel("");
+
+            Assert.IsNull(actualResult);
+        }
+
+        [Test]
+        public void GetEditModelForCompany_ShouldReturn_NullWhenApplicationIsDeleted()
+        {
+            SeedDatabase();
+
+            var id = applicationService.Add(
+              internshipId,
+              candidateId,
+              "This is cover letter This is a cover letter This is a cover letter");
+
+            applicationService.Delete(id);
+
+            var actualResult = applicationService.GetEditModel(id);
+
+            Assert.IsNull(actualResult);
+        }
+
+        [Test]
+        public void GetEditModelForCompany_ShouldReturn_ApplicationFormModel()
+        {
+            SeedDatabase();
+
+            var id = applicationService.Add(
+              internshipId,
+              candidateId,
+              "This is cover letter This is a cover letter This is a cover letter");
+
+            var expectedModel = new ApplicationFormModel
+            {
+                Id = id,
+                InternshipId = internshipId,
+                CandidateId = candidateId,
+                CoverLetter = "This is cover letter This is a cover letter This is a cover letter"
+            };
+
+            var actualModel = applicationService.GetEditModel(id);
+
+            Assert.That(actualModel, Has.Property("Id").EqualTo(expectedModel.Id)
+                                   & Has.Property("InternshipId").EqualTo(expectedModel.InternshipId)
+                                   & Has.Property("CandidateId").EqualTo(expectedModel.CandidateId)
+                                   & Has.Property("CoverLetter").EqualTo(expectedModel.CoverLetter));
+        }
+
+        [Test]
+        public void GetInternshipApplicants_ShouldReturn_InternshipApplicantListingQueryModel_WithEmptyApplicantsCollection_WhenInternshipIdDoesNotMatch()
+        {
+            var actualResult = applicationService.GetInternshipApplicants(
+                null,
+                null,
+                null,
+                null,
+                "",
+                null,
+                1,
+                5);
+
+            Assert.IsEmpty(actualResult.Applicants);
+        }
+
+        [Test]
+        public void GetInternshipApplicants_ShouldReturn_InternshipApplicantListingQueryModel_WithEmptyApplicantsCollection_WhenInternshipIsDeleted()
+        {
+            SeedDatabase();
+
+            var id = applicationService.Add(
+              internshipId,
+              candidateId,
+              "This is cover letter This is a cover letter This is a cover letter");
+
+            applicationService.Delete(id);
+
+            var actualResult = applicationService.GetInternshipApplicants(
+               null,
+               null,
+               null,
+               null,
+               internshipId,
+               null,
+               1,
+               5);
+
+            Assert.IsEmpty(actualResult.Applicants);
+        }
+
+        [Test]
+        public void GetInternshipApplicants_ShouldReturn_InternshipApplicantListingQueryModel_WithFetchedInternshipRole_WhenItIsNullOrEmpty()
+        {
+            SeedDatabase();
+
+            applicationService.Add(
+              internshipId,
+              candidateId,
+              "This is cover letter This is a cover letter This is a cover letter");
+
+            var actualResult = applicationService.GetInternshipApplicants(
+               null,
+               null,
+               null,
+               null,
+               internshipId,
+               null,
+               1,
+               5);
+
+            Assert.That(actualResult.InternshipRole, Is.EqualTo("Junior Full-Stack Developer"));
+        }
+
+        [Test]
+        public void GetInternshipApplicants_ShouldSanitizeApplicantFirstName_AndReturn_InternshipApplicantListingQueryModel_WithFilteredApplicantsCollection()
+        {
+            SeedDatabase();
+
+            applicationService.Add(
+             internshipId,
+             candidateId,
+             "This is cover letter This is a cover letter This is a cover letter");
+
+            var actualResultOne = applicationService.GetInternshipApplicants(
+               "  <script>Emre</script>     ",
+               null,
+               null,
+               null,
+               internshipId,
+               "Junior Full-Stack Developer",
+               1,
+               5);
+
+            var actualResultTwo = applicationService.GetInternshipApplicants(
+               "  <script>ThisWholeSectionWithTheScriptTagsWillBeRemovedAsWellAsTrimAllSpaces</script>     ",
+               null,
+               null,
+               null,
+               internshipId,
+               "Junior Full-Stack Developer",
+               1,
+               5);
+
+            var actualResultThree = applicationService.GetInternshipApplicants(
+               "  Em     ",
+               null,
+               null,
+               null,
+               internshipId,
+               "Junior Full-Stack Developer",
+               1,
+               5);
+
+            var actualResultFour = applicationService.GetInternshipApplicants(
+               "  Emrrrreeee     ",
+               null,
+               null,
+               null,
+               internshipId,
+               "",
+               1,
+               5);
+
+            Assert.That(actualResultOne.Applicants.Count(), Is.EqualTo(1));
+            Assert.That(actualResultTwo.Applicants.Count(), Is.EqualTo(1));
+            Assert.That(actualResultThree.Applicants.Count(), Is.EqualTo(1));
+            Assert.That(actualResultFour.Applicants.Count(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GetInternshipApplicants_ShouldSanitizeApplicantLastName_AndReturn_InternshipApplicantListingQueryModel_WithFilteredApplicantsCollection()
+        {
+            SeedDatabase();
+
+            applicationService.Add(
+             internshipId,
+             candidateId,
+             "This is cover letter This is a cover letter This is a cover letter");
+
+            var actualResultOne = applicationService.GetInternshipApplicants(
+               null,
+               " <script>Ereceb</script>     ",
+               null,
+               null,
+               internshipId,
+               "Junior Full-Stack Developer",
+               1,
+               5);
+
+            var actualResultTwo = applicationService.GetInternshipApplicants(
+               null,
+               "  <script>ThisWholeSectionWithTheScriptTagsWillBeRemovedAsWellAsTrimAllSpaces</script>     ",
+               null,
+               null,
+               internshipId,
+               "Junior Full-Stack Developer",
+               1,
+               5);
+
+            var actualResultThree = applicationService.GetInternshipApplicants(
+               null,
+               "  Er     ",
+               null,
+               null,
+               internshipId,
+               "Junior Full-Stack Developer",
+               1,
+               5);
+
+            var actualResultFour = applicationService.GetInternshipApplicants(
+               null,
+               "  Errrreeee     ",
+               null,
+               null,
+               internshipId,
+               "",
+               1,
+               5);
+
+            Assert.That(actualResultOne.Applicants.Count(), Is.EqualTo(1));
+            Assert.That(actualResultTwo.Applicants.Count(), Is.EqualTo(1));
+            Assert.That(actualResultThree.Applicants.Count(), Is.EqualTo(1));
+            Assert.That(actualResultFour.Applicants.Count(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GetInternshipApplicants_ShouldFilterWithApplicantSpecializationId_AndReturn_InternshipApplicantListingQueryModel_WithFilteredApplicantsCollection()
+        {
+            SeedDatabase();
+
+            applicationService.Add(
+             internshipId,
+             candidateId,
+             "This is cover letter This is a cover letter This is a cover letter");
+
+            var actualResultOne = applicationService.GetInternshipApplicants(
+               null,
+               null,
+               specializationId,
+               null,
+               internshipId,
+               "Junior Full-Stack Developer",
+               1,
+               5);
+
+            var actualResultTwo = applicationService.GetInternshipApplicants(
+               "Em",
+               null,
+               specializationId,
+               null,
+               internshipId,
+               "Junior Full-Stack Developer",
+               1,
+               5);
+
+            var actualResultThree = applicationService.GetInternshipApplicants(
+               "Em",
+               null,
+               "30r92j03r9230rj3r039rj",
+               null,
+               internshipId,
+               "Junior Full-Stack Developer",
+               1,
+               5);
+
+            Assert.That(actualResultOne.Applicants.Count(), Is.EqualTo(1));
+            Assert.That(actualResultTwo.Applicants.Count(), Is.EqualTo(1));
+            Assert.That(actualResultThree.Applicants.Count(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GetInternshipApplicants_ShouldFilterWithApplicantCountryId_AndReturn_InternshipApplicantListingQueryModel_WithFilteredApplicantsCollection()
+        {
+            SeedDatabase();
+
+            applicationService.Add(
+             internshipId,
+             candidateId,
+             "This is cover letter This is a cover letter This is a cover letter");
+
+            var actualResultOne = applicationService.GetInternshipApplicants(
+               null,
+               null,
+               null,
+               countryId,
+               internshipId,
+               "Junior Full-Stack Developer",
+               1,
+               5);
+
+            var actualResultTwo = applicationService.GetInternshipApplicants(
+               "Em",
+               null,
+               null,
+               countryId,
+               internshipId,
+               "Junior Full-Stack Developer",
+               1,
+               5);
+
+            var actualResultThree = applicationService.GetInternshipApplicants(
+               "Em",
+               null,
+               null,
+               "dfwpeofkowpeof3ef",
+               internshipId,
+               "Junior Full-Stack Developer",
+               1,
+               5);
+
+            Assert.That(actualResultOne.Applicants.Count(), Is.EqualTo(1));
+            Assert.That(actualResultTwo.Applicants.Count(), Is.EqualTo(1));
+            Assert.That(actualResultThree.Applicants.Count(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GetInternshipApplicants_ShouldReturn_InternshipApplicantListingQueryModel_WithCorrectPageAndPerPageProperties()
+        {
+            var actualResult = applicationService.GetInternshipApplicants(
+              "Em",
+              null,
+              null,
+              "dfwpeofkowpeof3ef",
+              internshipId,
+              "Junior Full-Stack Developer",
+              -1,
+              -5);
+
+            var actualResultTwo = applicationService.GetInternshipApplicants(
+              "Em",
+              null,
+              null,
+              "dfwpeofkowpeof3ef",
+              internshipId,
+              "Junior Full-Stack Developer",
+              0,
+              100);
+
+            Assert.That(actualResult.CurrentPage, Is.EqualTo(1));
+            Assert.That(actualResult.ApplicantsPerPage, Is.EqualTo(6));
+
+            Assert.That(actualResultTwo.CurrentPage, Is.EqualTo(1));
+            Assert.That(actualResultTwo.ApplicantsPerPage, Is.EqualTo(96));
         }
 
         private void SeedDatabase()
@@ -475,10 +866,14 @@
                 Name = "Software Development"
             };
 
+            specializationId = specialization.Id;
+
             var country = new Country
             {
                 Name = "Bulgaria"
             };
+
+            countryId = country.Id;
 
             data.Specializations.Add(specialization);
             data.Countries.Add(country);
