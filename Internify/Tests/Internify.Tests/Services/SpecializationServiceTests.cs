2754171a612bc894679184547d0a148b0f7b104e
emrrar <avatareigan@gmail.com>
2022-03-29 15:44:58 +0300

    Finished SpecializationServiceTests

---
+++ Internify/Tests/Internify.Tests/Services/SpecializationServiceTests.cs
@@ -0,0 +1,83 @@
+﻿namespace Internify.Tests.Services
+{
+    using Data;
+    using Data.Models;
+    using Internify.Services.Specialization;
+    using Microsoft.EntityFrameworkCore;
+    using NUnit.Framework;
+    using System.Linq;
+
+    public class SpecializationServiceTests
+    {
+        private InternifyDbContext data;
+        private ISpecializationService specializationService;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<InternifyDbContext>()
+                .UseInMemoryDatabase("MyInMemoryDatabase")
+                .Options;
+
+            data = new InternifyDbContext(options);
+            data.Database.EnsureCreated();
+            specializationService = new SpecializationService(data);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            data.Database.EnsureDeleted();
+        }
+
+        [Test]
+        public void All_ShouldReturn_EmptyCollection()
+        {
+            var actualResult = specializationService.All();
+
+            Assert.IsEmpty(actualResult);
+        }
+
+        [Test]
+        public void All_ShouldReturn_SpecializationListingViewModel()
+        {
+            var specialization = new Specialization
+            {
+                Name = "Software Development"
+            };
+
+            data.Specializations.Add(specialization);
+            data.SaveChanges();
+
+            var actualFirstSpecializationName = specializationService
+                .All()
+                .FirstOrDefault()?.Name;
+
+            Assert.That(actualFirstSpecializationName, Is.EqualTo(specialization.Name));
+        }
+
+        [Test]
+        public void Exists_ShouldReturn_False()
+        {
+            var actualResult = specializationService.Exists("fwefw");
+
+            Assert.IsFalse(actualResult);
+        }
+
+        [Test]
+        public void Exists_ShouldReturn_True()
+        {
+            var specialization = new Specialization
+            {
+                Name = "Software Development"
+            };
+
+            data.Specializations.Add(specialization);
+            data.SaveChanges();
+
+            var actualResult = specializationService.Exists(specialization.Id);
+
+            Assert.IsTrue(actualResult);
+        }
+    }
+}
