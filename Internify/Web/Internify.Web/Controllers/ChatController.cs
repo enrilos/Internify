904a1ba6326029175e88c0f343fef4525533307d
emrrar <avatareigan@gmail.com>
2022-04-03 20:45:05 +0300

    Added ChatController along with All view.

---
+++ Internify/Web/Internify.Web/Controllers/ChatController.cs
@@ -0,0 +1,22 @@
+﻿namespace Internify.Web.Controllers
+{
+    using Services.Data.Message;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize]
+    public class ChatController : Controller
+    {
+        private readonly IMessageService messageService;
+
+        public ChatController(IMessageService messageService)
+            => this.messageService = messageService;
+
+        public IActionResult All()
+        {
+            var messages = messageService.GetLatestHundredMessages();
+
+            return View(messages);
+        }
+    }
+}
