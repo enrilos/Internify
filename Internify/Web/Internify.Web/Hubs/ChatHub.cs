4571423ff9a56840f57d96d58d740dd38cfe637a
emrrar <avatareigan@gmail.com>
2022-04-03 20:44:13 +0300

    Added ChatHub.

---
+++ Internify/Web/Internify.Web/Hubs/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿namespace Internify.Web.Hubs
+{
+    using Services.Data.Message;
+    using Microsoft.AspNetCore.SignalR;
+
+    public class ChatHub : Hub
+    {
+        private readonly IMessageService messageService;
+
+        public ChatHub(IMessageService messageService)
+            => this.messageService = messageService;
+
+        public async Task SendMessage(string user, string message)
+        {
+            var sanitizedMessage = messageService.Add(user, message);
+
+            if (sanitizedMessage == null)
+            {
+                return;
+            }
+
+            await Clients.All.SendAsync("ReceiveMessage", user, sanitizedMessage);
+        }
+    }
+}
