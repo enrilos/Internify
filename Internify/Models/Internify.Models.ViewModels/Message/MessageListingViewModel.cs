bd71d27aeb9337edfd127789774653b63ba22763
emrrar <avatareigan@gmail.com>
2022-04-03 20:41:40 +0300

    Added MessageListingViewModel

---
+++ Internify/Models/Internify.Models.ViewModels/Message/MessageListingViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Internify.Models.ViewModels.Message
+{
+    public class MessageListingViewModel
+    {
+        public string Content { get; init; }
+
+        public string SenderEmail { get; init; }
+
+        public DateTime SentOn { get; init; }
+    }
+}
