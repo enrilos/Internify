6877cb6b6be47a6533748484644edf612dd71f42
emrrar <avatareigan@gmail.com>
2022-04-07 18:30:22 +0300

    Introduced Mapbox in conjunction with Geolocation API.

---
+++ Internify/Web/Internify.Web/wwwroot/js/mapbox.js
@@ -0,0 +1,23 @@
+﻿mapboxgl.accessToken = process.env.MAPBOX_API_KEY;
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, { enableHighAccuracy: true });
+
+function successLocation(position) {
+    setupMap([position.coords.longitude, position.coords.latitude]);
+}
+
+function errorLocation() {
+    document.querySelector('#map').remove();
+}
+
+const setupMap = (center) => {
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: center,
+        zoom: 15
+    });
+
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav);
+}
